94e4355adbdb7688484d9a6bd1e0cfdf0cfb3d40
OZO <royhsu1012@gmail.com>
2021-07-06 15:02:29 +0800

    Delete Task_List directory

--- Task_List/server/services/render.js
+++
@@ -1,9 +0,0 @@
-exports.homeRoutes=(req,res)=>{
-    res.render("index");
-}
-exports.add_task=(req,res)=>{
-    res.render('add_task');
-}
-exports.update_task=(req,res)=>{
-    res.render('update_task');
-}
